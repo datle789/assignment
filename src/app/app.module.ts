26b080c4550b4838598722e431a51996ff4f8410
Admin <datletb789@gmail.com>
2024-08-26 16:33:01 +0700

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { CreateBookComponent } from './create-book/create-book.component';
+import { UpdateBookComponent } from './update-book/update-book.component';
+import { ViewBookComponent } from './view-book/view-book.component';
+import { EnumDescriptionPipe } from './enum-description.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    CreateBookComponent,
+    UpdateBookComponent,
+    ViewBookComponent,
+    EnumDescriptionPipe
   ],
   imports: [
     BrowserModule,
